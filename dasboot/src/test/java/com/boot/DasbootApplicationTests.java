b805a3a2836e21ed87105b5bb0408ce8ea66d4e3
fabgitlocal <fmiredin7@gmail.com>
2019-09-13 19:38:46 +0200

    add configuration persisting (!= in memory) database H2 with absolute path

--- dasboot/src/test/java/com/boot/DasbootApplicationTests.java
+++ dasboot/src/test/java/com/boot/DasbootApplicationTests.java
@@ -1,5 +1,6 @@
 package com.boot;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DasbootApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void testCcontextLoads() {
     }
 
 }
