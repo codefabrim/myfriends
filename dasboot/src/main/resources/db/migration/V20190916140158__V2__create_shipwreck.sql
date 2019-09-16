28823351b4423d387f5d1ff0fc2307c374c05a58
fabgitlocal <fmiredin7@gmail.com>
2019-09-16 14:23:00 +0200

    flyway: integrate flywayDB

---
+++ dasboot/src/main/resources/db/migration/V20190916140158__V2__create_shipwreck.sql
@@ -0,0 +1,11 @@
+create table shipwreck(
+                          ID int auto_increment,
+                          name varchar(255),
+                          description varchar(2000),
+                          condition varchar(255),
+                          depth int,
+                          latitude double,
+                          longitude double,
+                          year_discovery int
+)
+
