5693135639a07ae8e20c43e54cd216b6c71aa4ac
fabgitlocal <fmiredin7@gmail.com>
2019-09-17 18:57:59 +0200

    initial config

---
+++ demoSpringRest/src/test/java/com/example/demoSpringRest/DemoSpringRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demoSpringRest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
