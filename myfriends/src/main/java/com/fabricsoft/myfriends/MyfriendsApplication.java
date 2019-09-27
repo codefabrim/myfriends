2bf43e4deaed01c3ea97064706df59b2085f2c81
fabgitlocal <fmiredin7@gmail.com>
2019-09-27 09:34:38 +0200

    myfriends: initial configuration pom.xml, file .gitignore"

---
+++ myfriends/src/main/java/com/fabricsoft/myfriends/MyfriendsApplication.java
@@ -0,0 +1,13 @@
+package com.fabricsoft.myfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfriendsApplication.class, args);
+	}
+
+}
